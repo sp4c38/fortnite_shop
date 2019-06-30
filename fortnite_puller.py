d8e9dd80a1bebb965be0d3e52ee6011eee23e18e
Léon Becker <lb@space8.me>
2019-06-30 21:38:23 +0200

    Now merges intelligent multiple photos to one

--- fortnite_puller.py
+++ fortnite_puller.py
@@ -1,60 +1,111 @@
 import datetime
+import io
 import os
-import PIL
 import random
 import requests
 import shutil
 import tempfile
 
+from PIL import Image
 from os.path import expanduser
 
+# Put in credentials file later
 chat_id = '-1001269378894'
 send_photo_link = 'https://api.telegram.org/bot723477855:AAEPsApC9-UXtWZ7QWZxndvRuY8ZXQoXM1g/sendPhoto'
 store_path = '~/Documents/fortnite_shop'
 
-url = 'https://fortnite-api.theapinetwork.com/store/get'
+shop_url = 'https://fnbr.co/api/shop'
 headers = {
-  'Authorization': '1b356241bf656c44c8ce44859be14391'
+  'x-api-key': 'dcf4fb2e-5e42-4938-9706-33e9283089e1'
 }
 
+# open requests session (more effectiv than to open one each time needed)
 requests_session = requests.Session()
-store_files = []
+
+# Change some stuff here if ya want to
+
+# Settings for images next to each other in a row
+row_images_next_to_each_other = 4 # How many images shall be next to each other?
+width = 400 # The width of each individual image / Width should be: width=height
+height = 400 # The height of each individual image / height should be: height=width
+# Settings for the final image
+final_img_bg = (0,0,255) # Please specify as RGB or RGBA
+
+images = []
+img_links = []
 
 # Give api key with in header (must be 'headers' in request)
-data = (requests_session.get(url=url, headers=headers)).json()
-store_links = []
+# Pulls json from api site with credentials and gets needed information
 
-for item in data['data']:
-    store_links.append(item['item']['images']['information'])
+data = (requests_session.get(url=shop_url, headers=headers)).json()
+for item in data['data']['featured']:
+    img_links.append(item['images']['icon'])
 
+for item in data['data']['daily']:
+    img_links.append(item['images']['icon'])
+
+
+# Creates name for directory (with date)
 date = datetime.date.today().timetuple()
 dir_created_date = "_".join([str(date.tm_mday), str(date.tm_mon), str(date.tm_year)])
 
+# Overwork later 
 if not os.path.exists(path=(expanduser(f'{store_path}/{dir_created_date}'))):
     os.makedirs(name=(expanduser(f'{store_path}/{dir_created_date}')))
 
 def generate_mkstemp():
+    # Generate a preferably unique name for a file
     imagefile_path = ((tempfile.mkstemp(prefix='fn_', suffix='.png',
                         dir=(expanduser('{0}/{1}'.format(store_path, dir_created_date)))))[1])
     return imagefile_path
 
-for link in store_links:
-    imagefile_path = generate_mkstemp()
-    while imagefile_path in store_files:
-        imagefile_path = generate_mkstemp()
-    store_files.append(imagefile_path)
-    with open(imagefile_path, 'wb') as f:
-        image = requests_session.get(url=link)
-        f.write(image.content)
-        print(f"Successfully wrote image to: {imagefile_path}")    
+print('Downloading...')
+for link in img_links:
+    # Saves image from website as PIL.Image
+    print(link)
+    images.append(Image.open(io.BytesIO(requests.get(url=link).content)))
 
-for i in store_files:
-    header = {
-    'chat_id': chat_id
-    }
-    file = {
-        'photo':open(i, 'rb')
-    }
-    requests.post(url=send_photo_link, data=header, files=file)
-print("Send all photos successfully.")
 
+def first_items(items_list, number):
+    x = 0
+    first_items = []
+    
+    for item in items_list[slice(number)]:
+        first_items.append(items_list.pop(items_list.index(item)))
+    
+    return first_items
+
+def img_to_row(img_list):
+    row_img = Image.new(mode='RGBA', size=((len(img_list)*width), height), color=(255,255,255))
+    x_paste=0
+    y_paste=0
+    for image in img_list:
+        image = image.resize(size=(width,height))
+        row_img.paste(im=image, box=(x_paste, y_paste))
+        x_paste += width
+    return row_img
+
+def rows_to_one(rows):
+    result_img = Image.new(mode='RGBA',color=(255,255,255),\
+                            size=((row_images_next_to_each_other*width),height*len(rows)))
+    x_paste=0
+    y_paste=0
+    for image in rows:
+        result_img.paste(im=image, box=(x_paste,y_paste),)
+        y_paste += height
+    return result_img
+
+images_sliced = []
+
+while len(images) > 0:
+    images_sliced.append(first_items(items_list=images, number=row_images_next_to_each_other))
+
+row_imgs = []
+# import IPython;IPython.embed();import sys;sys.exit()
+for item in images_sliced:
+    row_imgs.append(img_to_row(img_list=item))
+
+foreground = rows_to_one(rows=row_imgs)
+final = Image.new(color=final_img_bg, size=(int(foreground.width/2), int(foreground.height/2)), mode='RGBA')
+foreground = foreground.resize(size=(int(foreground.width/2), int(foreground.height/2)))
+final.paste(im=foreground,box=(0,0),mask=foreground)
