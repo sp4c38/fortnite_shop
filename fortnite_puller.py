09abb4d7725bee996e5d59bd99a62ca8f218408d
Léon Becker <lb@space8.me>
2019-07-04 21:09:11 +0200

    Set foundation with NamedTuple to(see description)
    Set foundation with NamedTuple to save any data at start of program
    (which is pulled from the api) for later in program. Idea e.g.: Set
    background color of each item with help of the rarity grades. Have to
    have which rarity the item has in the middle of the program.

--- fortnite_puller.py
+++ fortnite_puller.py
@@ -11,9 +11,10 @@ import tempfile
 
 from os.path import expanduser
 from PIL import Image,ImageDraw,ImageFont
+from typing import NamedTuple
 
 # Put in credentials file later
-chat_id = '-1001269378894'
+chat_id = '-340145564'
 send_photo_link = 'https://api.telegram.org/bot723477855:AAEPsApC9-UXtWZ7QWZxndvRuY8ZXQoXM1g/sendPhoto'
 
 shop_url = 'https://fnbr.co/api/shop'
@@ -44,20 +45,24 @@ height = 512 # The height of each individual image / height should be: height=wi
 # Settings for the final image
 final_img_bg = (255,102,0) # Please specify as RGB or RGBA
 
-images = []
-img_links = []
-
 # Give api key with in header (must be 'headers' in request)
 # Pulls json from api site with credentials and gets needed information
 
 data = (requests_session.get(url=shop_url, headers=headers)).json()
+data_img_link = [] # Items in list as NamedTuple
+data_img = [] # Items in list as NamedTuple
 
 for item in data['data']['featured']:
-    img_links.append(item['images']['icon'])
+    part_data_img_link = NamedTuple('img', [('img_url', str),('rarity', str)])
+    part_data_img_link.img_link = item['images']['icon']
+    part_data_img_link.rarity = item['rarity']
+    data_img_link.append(part_data_img_link)
 
 for item in data['data']['daily']:
-    img_links.append(item['images']['icon'])
-
+    part_data_img_link = NamedTuple('url_and_rarity', [('img_url', str),('rarity', str)])
+    part_data_img_link.img_link = item['images']['icon']
+    part_data_img_link.rarity = item['rarity']
+    data_img_link.append(part_data_img_link)
 
 # Creates name for directory (with date)
 date = datetime.date.today().timetuple()
@@ -67,30 +72,33 @@ if not os.path.exists(path=(f'{backups_store_path}/{dir_name}')):
     os.makedirs(name=(f'{backups_store_path}/{dir_name}'))
 
 print('Downloading...')
-for link in img_links:
+for i in data_img_link:
+    part_data_img = NamedTuple('image_and_rarity', [('img', str), ('rarity', str)])
     # Saves image from website as PIL.Image
-    print(link)
-    images.append(Image.open(io.BytesIO(requests.get(url=link).content)))
-
+    part_data_img.rarity=i.rarity
+    print(i.img_link)
+    part_data_img.img = Image.open(io.BytesIO(requests.get(url=i.img_link).content))
+    data_img.append(part_data_img)
 
 def first_items(items_list, number):
     x = 0
     first_items = []
-    
+
     for item in items_list[slice(number)]:
         first_items.append(items_list.pop(items_list.index(item)))
     
     return first_items
 
-def img_to_row(img_list):
+def imgs_to_row(img_list):
     row_img = Image.new(mode='RGBA', size=((len(img_list)*width), height), color=final_img_bg)
     
     x_paste=0
     y_paste=0
-    
-    for image in img_list:
-        row_img.paste(im=image, box=(x_paste, y_paste))
+
+    for item in img_list:
+        row_img.paste(im=item.img, box=(x_paste, y_paste))
         x_paste += width
+
 
     return row_img
 
@@ -99,9 +107,11 @@ def rows_to_one(rows):
                             size=((row_images_next_to_each_other*width),height*len(rows)))
     x_paste=0
     y_paste=0
+    
     for image in rows:
         result_img.paste(im=image, box=(x_paste,y_paste))
         y_paste += height
+    
     return result_img
 
 def send_img_as_telegram_message():
@@ -166,14 +176,15 @@ def edit_single_image(image_2_edit):
 images_sliced = []
 row_imgs = []
 
-for image in images:
-    images[images.index(image)] = edit_single_image(image_2_edit=image)
+for i in data_img:
+    img = i.img
+    data_img[data_img.index(i)].img = edit_single_image(image_2_edit=img)
 
-while len(images) > 0:
-    images_sliced.append(first_items(items_list=images, number=row_images_next_to_each_other))
+while len(data_img) > 0:
+    images_sliced.append(first_items(items_list=data_img, number=row_images_next_to_each_other))
 
-for item in images_sliced:
-    row_imgs.append(img_to_row(img_list=item))
+for items_as_list in images_sliced:
+    row_imgs.append(imgs_to_row(img_list=items_as_list))
 
 final = rows_to_one(rows=row_imgs)
 
