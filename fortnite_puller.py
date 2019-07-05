4a9ef1258fd53451d6dfa0324c4af916ddf07f9f
Léon Becker <lb@space8.me>
2019-07-05 21:56:59 +0200

    Now shows the price of the item and...
    Now shows the price of each item of the shop in the left upper corner.
    Now changes the background of each item of the shop dependently on its
    rarity. E.g.: uncommon -> background color green

--- fortnite_puller.py
+++ fortnite_puller.py
@@ -15,7 +15,7 @@ from typing import NamedTuple
 
 # Put in credentials file later
 
-chat_id = '-340145564' 
+chat_id = '-1001323616343'
 
 send_photo_link = 'https://api.telegram.org/bot723477855:AAEPsApC9-UXtWZ7QWZxndvRuY8ZXQoXM1g/sendPhoto'
 
@@ -30,14 +30,26 @@ requests_session = requests.Session()
 # Change some stuff here if ya want to
 
 # Some general settings
+rarity_grades = {
+    'common':(128,128,128), # gray
+    'uncommon': (11, 165, 52), # green
+    'rare': (0,0,255), # blue
+    'epic': (128,0,128), # purple
+    'legendary': (255,102,0) # orange
+}
 backups_store_path = expanduser(os.path.join('~', 'Documents', 'fortnite_shop', 'backups'))
 store_path_final = expanduser(os.path.join('~', 'Documents', 'fortnite_shop' , 'backups', '{0}', 'final.png'))
-text_font_path = expanduser(os.path.join('~', 'Documents', 'fortnite_shop', 'fonts', 'Lato-Bold.ttf')) # Please set as .ttf file
 
+text_font_path = expanduser(os.path.join('~', 'Documents', 'fortnite_shop', 'fonts', 'Lato-Bold.ttf')) # Please set as .ttf file
+character_space_to_top = 6 # If your font characters have a certain space to the top (from each character to the end of the top)
+                            # please specifiy this space in px, it's important to that the calculation works like it should
+                
 # Settings for vbucks text (for each image)
-vbucks_text_size = 50
-vbucks_text_position = (0,0)
+vbucks_text_size = 40 #  Will resize the vbucks image according to this setting
 vbucks_text_color = (255,255,255)
+spacing_to_top = 5
+spacing_to_side = 5 # spacing to edge of the vbucks image in pixel (will adjust the text field automatically)
+vbucks_img_path = expanduser(os.path.join('~', 'Documents', 'fortnite_shop', 'data', 'vbucks_icon', 'icon_vbucks.png'))
 
 
 # Settings for images next to each other in a row
@@ -55,15 +67,27 @@ data_img_link = [] # Items in list as NamedTuple
 data_img = [] # Items in list as NamedTuple
 
 for item in data['data']['featured']:
-    part_data_img_link = NamedTuple('img', [('img_url', str),('rarity', str)])
-    part_data_img_link.img_link = item['images']['icon']
+    part_data_img_link = NamedTuple('img', [('img_url', str),('rarity', str),('price', str)])
+
+    if item['images']['icon']:
+        part_data_img_link.img_link = item['images']['icon']
+    else:
+        part_data_img_link.img_link = item['images']['icon']
+
     part_data_img_link.rarity = item['rarity']
+    part_data_img_link.price = item['price']
     data_img_link.append(part_data_img_link)
 
 for item in data['data']['daily']:
-    part_data_img_link = NamedTuple('url_and_rarity', [('img_url', str),('rarity', str)])
-    part_data_img_link.img_link = item['images']['icon']
+    part_data_img_link = NamedTuple('url_and_rarity', [('img_url', str),('rarity', str),('price', str)])
+    
+    if item['images']['icon']:
+        part_data_img_link.img_link = item['images']['icon']
+    else:
+        part_data_img_link.img_link = item['images']['icon']
+    
     part_data_img_link.rarity = item['rarity']
+    part_data_img_link.price = item['price']
     data_img_link.append(part_data_img_link)
 
 # Creates name for directory (with date)
@@ -75,9 +99,10 @@ if not os.path.exists(path=(f'{backups_store_path}/{dir_name}')):
 
 print('Downloading...')
 for i in data_img_link:
-    part_data_img = NamedTuple('image_and_rarity', [('img', str), ('rarity', str)])
+    part_data_img = NamedTuple('data', [('img', str), ('rarity', str),('price', str)])
     # Saves image from website as PIL.Image
-    part_data_img.rarity=i.rarity
+    part_data_img.rarity= i.rarity
+    part_data_img.price = i.price
     print(i.img_link)
     part_data_img.img = Image.open(io.BytesIO(requests.get(url=i.img_link).content))
     data_img.append(part_data_img)
@@ -148,30 +173,44 @@ def check_if_changed(final_img, saved_imgs_path):
         final.save(fp=store_path_final.format(dir_name))
         send_img_as_telegram_message()
 
-def edit_single_image(image_2_edit):
-    image_2_edit = image_2_edit.resize(size=(width,height))
+def edit_single_image(img_data):
+    img_data.img = img_data.img.resize(size=(width,height))
 
     x_paste = 0
     y_paste = 0
 
+    if not os.path.exists(text_font_path):
+        print(f"Font path \'{text_font_path}\' doesn't exist.")
+        sys.exit(1)
+
     edited_image = Image.new(size=(width, height), mode='RGBA')
-    edited_image.paste(im=image_2_edit, box=(0,0))
-
-    # if not os.path.exists(text_font_path):
-    #     print(f"Font path \'{text_font_path}\' doesn't exist.")
-    #     sys.exit(1)
-
+    edited_image.paste(im=img_data.img, box=(0,0))
     
-    # draw = ImageDraw.Draw(edited_image)
-    # font = ImageFont.truetype(font=text_font_path, size=vbucks_text_size)
-    # draw.text(xy=vbucks_text_position, text='1500', fill=vbucks_text_color,\
-    #           font=font)
+
+    # Add v-bucks price for shop itme
+    vbucks_icon = Image.open(fp=vbucks_img_path)
+    vbucks_icon = vbucks_icon.resize(size=(vbucks_text_size,vbucks_text_size))
+
+    image_position = (spacing_to_side, spacing_to_top)
+    #text_position = ((spacing_to_side+vbucks_icon.width+10), (((spacing_to_top+(vbucks_icon.height/2))-(vbucks_text_size/2))-character_space_to_top))
+    text_position = ((spacing_to_side+vbucks_icon.width+10),spacing_to_top-character_space_to_top)
+    edited_image.paste(im=vbucks_icon, box=image_position)
+
+    draw = ImageDraw.Draw(edited_image)
+    font = ImageFont.truetype(font=text_font_path, size=vbucks_text_size)
+    draw.text(xy=text_position, text=img_data.price, fill=vbucks_text_color,\
+              font=font)
+
+
+    # Set background color (dependet on the rarity of the shop item (image))
+    if img_data.rarity in rarity_grades:
+        rarity_color = rarity_grades[img_data.rarity]
 
     # Has to be pasted again on a new image so that the mask option works correctly (this type
     # of bug only would happens by some images (here not anymore (because it was fixed)))
-    bg_image = Image.new(size=(width, height), color=final_img_bg, mode='RGBA')
-    bg_image.paste(im=edited_image, box=(0,0), mask=edited_image)
-    
+    bg_image = Image.new(size=(width, height), color=rarity_color, mode='RGBA')
+    bg_image.paste(im=edited_image, box=(10,0), mask=edited_image)
+
     return bg_image
 
 
@@ -179,8 +218,8 @@ images_sliced = []
 row_imgs = []
 
 for i in data_img:
-    img = i.img
-    data_img[data_img.index(i)].img = edit_single_image(image_2_edit=img)
+    i
+    data_img[data_img.index(i)].img = edit_single_image(img_data=i)
 
 while len(data_img) > 0:
     images_sliced.append(first_items(items_list=data_img, number=row_images_next_to_each_other))
